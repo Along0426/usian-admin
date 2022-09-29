2420a3797cff912d78c8ec660bbb86f2e8ffa3c6
Along0426 <2798902906@qq.com>
2022-09-29 17:21:14 +0800

    feat(custom): 项目基础配置以及跨域处理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // 端口号
+    port: 6666,
+    // 主机名
+    host: 'localhost',
+    // 是否自动打开浏览器
+    open: false,
+    // 是否开启https
+    https: false,
+    // 配置跨域
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
