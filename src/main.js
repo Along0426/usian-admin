bf4596587f3c9d9297f21e4fc942439a435b2e46
Along0426 <2798902906@qq.com>
2022-09-29 17:31:29 +0800

    feat(custom): 引入需要使用的外部插件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// element-ui引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// axios引入
+import axios from 'axios'
+Vue.use(ElementUI)
+
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
